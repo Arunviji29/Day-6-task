fdce2e8c987ba3b010f1733544bac784fac71836
Arunviji29 <arunviji146@gmail.com>
2024-07-25 14:50:22 +0530

    updated

--- script.js
+++ script.js
@@ -1,9 +1,35 @@
-class movie {
-    constructor(title,studio,rating="PG"){
-        this.title = title;
-        this.studio = studio;
-        this.rating = rating;
+
+class Person{
+    constructor(name, age, gender, state, country){
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.state = state;
+        this.country = country;
     }
 }
-const obj = new movie("Leo","7-screen studio","7.5");
-console.log(obj.title,obj.studio,obj.rating);
+
+let person = new Person("Arun", 25, "Male", "Tamil nadu", "India");
+console.log(`3.Write a “person” class to hold all the details.
+${person.name}
+${person.age}
+${person.gender}
+${person.state}
+${person.country}
+`)
+
+// 4.write a class to calculate the uber price.
+
+const uber = {
+    set kiloMeter(km){    
+       this.price = `${km} km = Rs.${km * 50}` ;
+    },
+    get kiloMeter(){
+       return this.price;
+    }
+}
+
+uber.kiloMeter = 2;
+
+console.log(`4.) write a class to calculate the uber price.
+Answer : ${uber.kiloMeter}`)
